b9d118fc0891ce0679b679b322f8ce1b70d09743
xugang <xugang@feeyo.com>
2015-09-02 12:29:38 +0800

    replace _

--- charset.php
+++ charset.php
@@ -3,15 +3,10 @@ $handle = opendir('./');
 while (false !== ($file = readdir($handle))){
     if (pathinfo('./'.$file)['extension'] == 'htm') {
         $content = file_get_contents('./'.$file);
-        $content = iconv('GB2312', 'UTF-8', $content);
-        if ($content !== false) {
-            $content = str_replace('charset=gb2312', 'charset=UTF-8', $content);
-            $statu = file_put_contents('./'.$file, $content);
-            if ($statu != false) {
-                echo $file."success\n";
-            }
-        } else {
-            echo "iconv fair\n";
+        $content = preg_replace('|"_(\d*.htm)"|U', '${1}' , $content);
+        $statu = file_put_contents('./'.$file, $content);
+        if ($statu != false) {
+            echo $file."success\n";
         }
     }
 }
